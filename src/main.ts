f7ce28c027918752db392d071f049c8e3aee1e1e
emran-niftyitsolution <emran@niftyitsolution.com>
2025-07-15 19:15:22 +0600

    Enhance logging in bootstrap function of NestJS application
    - Added a Logger instance to provide structured logging during application startup.
    - Improved error handling by using Logger for error messages instead of console.error.
    - Included server status and GraphQL playground URL in the log output.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap', { timestamp: true });
+
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(
+    '-------------------------------------------',
+    `Server is running on port ${process.env.PORT ?? 3000}`,
+    `GraphQL playground: http://localhost:${process.env.PORT ?? 3000}/graphql`,
+    '-------------------------------------------',
+  );
 }
 bootstrap().catch((err) => {
-  console.error(err);
+  Logger.error(err);
   process.exit(1);
 });
